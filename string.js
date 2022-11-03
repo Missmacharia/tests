7185e3ed7dcf67f748e9cb8cfb1d1c280356d371
Missmacharia <yvonnemacharia30@gmail.com>
2022-11-03 04:01:23 +0300

    different ways of summing an array

---
+++ string.js
@@ -0,0 +1,33 @@
+// let word = 'Lorem at';
+// let secondWord= 'Tempor ip'
+// let word1 = word.split('').reverse().join('')
+// console.log(word1);
+// let myWord= secondWord.split('').reverse().join('')
+// console.log(myWord);
+
+// silent , listen 
+// let word1 = 'house';
+// let word2= 'mouse';
+
+// let  array1 = word1.split('').sort().join('')
+// let  array2 = word2.split('').sort().join('')
+
+// if (array1 === array2 ){
+//     console.log(true);
+// } else{
+//     console.log(false);
+// }
+
+
+//reversing  a string
+
+function strword(str){
+
+    let words= str.split('')
+
+    for(let i of words)
+
+    console.log(strword(i)+ '');
+}
+// let str='Lorem at';
+strword('Lorem at')
